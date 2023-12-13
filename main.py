773d955993cbc8972f04689d57bcda4d228ff553
Peter Vislocky <lilv447@gmail.com>
2023-12-13 13:17:47 -0500

    further encapsulated code

--- main.py
+++ main.py
@@ -3,28 +3,25 @@ from components.instanceCounter import pdfExtract
 
 nltk.download('punkt')
 
+
+DEFAULT_PATH = r'C:\Users\lilv4\OneDrive\Documents\AA-BigBook-4th-Edition.pdf'
+
+
 def get_user_pdf_path():
     user_pdf = input('Enter the path of the pdf file you would like to search: ')
-    return   rf'{user_pdf}' if user_pdf else r'C:\Users\lilv4\OneDrive\Documents\AA-BigBook-4th-Edition.pdf'
+    return   rf'{user_pdf}' if user_pdf else DEFAULT_PATH
 
-def process_pdf(pdf_path):
+def analyze_pdf(pdf_path):
     full_book = pdfExtract(pdf_path)
     word_to_find = input('Enter a word: ')
     word_count = full_book.word_count(word_to_find)
     sentences_list = full_book.return_full_sentences(word_to_find)
     return word_to_find, word_count, sentences_list
-    
 
-def main():
-    while True:
-        print('Will return how many times a given word appears in a pdf\n'
-              'Press Enter to default to the Big Book of AA')
-        pdf_path = get_user_pdf_path()
-        word_to_find, word_count, sentences_list = process_pdf(pdf_path)
+def display_sentences(sentences_list):
         num = 0
         output_count = 0
-        print(f'{word_to_find} appears {word_count} times in this PDF\n')
-
+        
         while num < len(sentences_list):
             
             for i in range(20):
@@ -49,6 +46,15 @@ def main():
                 continue
         
         print(f'Showing {output_count} out of {len(sentences_list)} results')
+
+def main():
+    while True:
+        print('Will return how many times a given word appears in a pdf\n'
+              'Press Enter to default to the Big Book of AA')
+        pdf_path = get_user_pdf_path()
+        word_to_find, word_count, sentences_list = analyze_pdf(pdf_path)
+        print(f'{word_to_find} appears {word_count} times in this PDF\n')
+        display_sentences(sentences_list)
         
         run_again = input('Run the program again? (y/n) ')
         
