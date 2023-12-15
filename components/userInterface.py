2b4613b9f1fee51dd9f42cc03c4dfb2e6e603707
Peter Vislocky <lilv447@gmail.com>
2023-12-15 04:16:37 -0500

    started a GUI class

---
+++ components/userInterface.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import scrolledtext
+from components.instanceCounter import pdfExtract
+
+#TODO make this whole class. lol
+
+class GUI:
+
+    def __init__(self, root) -> None:
+        self.root = root
+        self.root.title = 'PDF Scanner'
+    
